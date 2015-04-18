dcb46a73cbb2e7454d17991f5ed9a3ac90cfd7b5
Josh Alexy <jalexy12@gmail.com>
2015-04-18 19:34:56 -0400

    changed database to postgres

--- app/assets/javascripts/components/Track.jsx.jsx
+++ app/assets/javascripts/components/Track.jsx.jsx
@@ -1,7 +1,19 @@
 var Track = React.createClass({
+
 	render: function(){
 		return(
-				<span>{this.props.name}</span>
+			<div>
+				<div className="col-sm-4">
+				    <h4>{this.props.data.name}</h4>
+				 </div>
+				 <div className="col-sm-1 playButton"><i onClick={this.playTrack} className="fa fa-play"></i></div> 
+				<div className="col-sm-4 trackProgress">
+				     <div className="progress">
+		  				<div className="progress-bar" role="progressbar" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">
+		  				</div>
+					  </div>
+				  </div>
+			</div>
 			)
 	}
 })
@@ -20,18 +32,11 @@ var TrackList = React.createClass({
 	render: function(){
 		var trackNodes = this.props.tracks.map(function(track, index){
 			return <li className="row">
-					  <div className="col-sm-2">
+						<audio src={track.preview_url}></audio>
+					  <div className="col-sm-2 trackIndex">
 					    {index}.
 					  </div>
-					  <div className="col-sm-4">
-					    <h4><Track name={track.name} /></h4>
-					   </div> 
-					   <div className="col-sm-2">
-					     <small>Preview</small>
-					    </div>
-					   <div className="col-sm-4">
-					    <progressBar value="0" max="30" />
-					   </div>
+					  <Track data={track} />
 				   </li>
 		
 		})
@@ -39,25 +44,3 @@ var TrackList = React.createClass({
 	}
 })
 
-progressBar = React.createClass({
-	getInitialState: function(){
-		return({
-			val: this.props.value,
-		    max: this.props.max
-		})
-	},
-
-	componentWillReceiveProps: function(nextProps){
-		this.setState({
-			value: nextProps.val,
-			max: nextProps.max
-		})
-	},
-	render: function(){
-		return (
-			<div className="progress">
-			  <div className="progress-bar" role="progressbar" valuenow={this.state.value} valuemax={this.state.max}>
-			  </div>
-			</div>)
-	}
-})
